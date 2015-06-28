585897103364505d982ceb1e4dcfd555e4f1a8a2
Gabrielle7 <gpo_12@hotmail.com>
2015-06-28 16:23:00 -0300

    Update LightSensor.ino

--- Sketches/LightSensor.ino
+++ Sketches/LightSensor.ino
@@ -7,19 +7,19 @@
 
 unsigned long SLEEP_TIME = 10000; // Sleep time between reads (in milliseconds)
 
-MySensor gw;
+MySensor ms;
 MyMessage msg(CHILD_ID, V_LIGHT_LEVEL);
 int lastLightLevel;
 
 void setup()  
 { 
-  gw.begin(NULL, NODE_ID;
+  ms.begin(NULL, NODE_ID;
 
   // Send the sketch version information to the gateway and Controller
-  gw.sendSketchInfo("Light Sensor", "1.0");
+  ms.sendSketchInfo("Light Sensor", "1.0");
 
   // Register all sensors to gateway (they will be created as child devices)
-  gw.present(CHILD_ID, S_LIGHT_LEVEL);
+  ms.present(CHILD_ID, S_LIGHT_LEVEL);
 }
 
 void loop()      
@@ -27,11 +27,8 @@ void loop()
   int lightLevel = (1023-analogRead(LIGHT_SENSOR_ANALOG_PIN))/10.23; 
   Serial.println(lightLevel);
   if (lightLevel != lastLightLevel) {
-      gw.send(msg.set(lightLevel));
+      ms.send(msg.set(lightLevel));
       lastLightLevel = lightLevel;
   }
-  gw.sleep(SLEEP_TIME);
+  ms.sleep(SLEEP_TIME);
 }
-
-
-
